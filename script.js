a54500f0542e5e84fdd344781b64fb2308091e12
aman6649 <62390446+aman6649@users.noreply.github.com>
2020-07-27 14:07:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,15 @@
+
 document.querySelector('.zone').addEventListener('click',function(){
-    var copyText = document.querySelector(".zone").value;
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    document.execCommand("copy");
-    alert("Copied the text: " + copyText.value);
-  });
+  let text =document.querySelector('span').innerText;
+  let input = document.createElement('input');
+  input.setAttribute('value',text);
+  document.body.appendChild(input);
+  input.select();
+  document.execCommand('copy');
+  document.body.removeChild(input);
+  alert('copied: '+ text);
+});
 
 
-// function copy(that){
-//     var inp =document.createElement('input');
-//     document.body.appendChild(inp)
-//     inp.value =that.textContent
-//     inp.select();
-//     document.execCommand('copy',false);
-//     inp.remove();
-//     }
-
-
-
-//   const span = document.querySelector("span");
-// span.onclick = function() {
-//   document.execCommand("copy");
-// }
-// span.addEventListener("copy", function(event) {
-//   event.preventDefault();
-//   if (event.clipboardData) {
-//     event.clipboardData.setData("text/plain", span.textContent);
-//     alert(event.clipboardData.getData("text"))
-//   }
-// });
 
 
